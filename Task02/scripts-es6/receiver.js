c454cbd80f0d19bb25591a8f23a3ffa43ff31189
AliaksandrVarachai <vorochai@gmail.com>
2016-04-15 18:56:29 +0300

    ES6 script is transformed to ES5 with Babel

---
+++ Task02/scripts-es6/receiver.js
@@ -0,0 +1,63 @@
+require("babel-polyfill");
+//require("babel-runtime");
+
+const responseFormat = "json";
+const apiKey = "883c9597c1166a747b0aae229fe79970:18:74940799";
+
+function* SectionGenerator() {
+	const sections = [
+		"home",
+		"world",
+		"national",
+		"politics",
+		"nyregion",
+		"business",
+		"opinion",
+		"technology",
+		"science",
+		"health",
+		"sports",
+		"arts",
+		"fashion",
+		"dining",
+		"travel",
+		"magazine",
+		"realestate"
+	];
+	for (let i = 0; i < sections.length; i++) {
+		yield sections[i];
+	}
+}
+
+window.sectionGenerator = SectionGenerator();
+
+class NewsPiece {
+	constructor(responseFormat, apiKey, section) {
+		this._responseFormat = responseFormat;
+		this._apiKey = apiKey;
+		this._section = section;
+		this.createUrl();
+	}
+	createUrl() {
+		this._url = `http://api.nytimes.com/svc/topstories/v1/${this._section}.${this._responseFormat}?api-key=${this._apiKey}`;
+	}
+	set section(section) {
+		this._section = section;
+		this.createUrl();
+	}
+	
+	get section() {
+		return this._section;
+	}
+	get url() {
+		return this._url;
+	}
+	get responseJSON() {
+		return fetch(this._url)
+			.then( response => response.json())
+			.catch( ex => console.log("NYT error: wrong url or section", ex));
+	}
+}
+
+window.newsPiece = new NewsPiece(responseFormat, apiKey, sectionGenerator.next().value);
+window.json = newsPiece.responseJSON;
