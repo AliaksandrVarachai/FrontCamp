b630fceff12ee54e7daecf86b233a741e1592999
AliaksandrVarachai <vorochai@gmail.com>
2016-06-01 09:25:48 +0200

    Task 7 is made on workshop

---
+++ Task07/controllers/articles-ctrl.js
@@ -0,0 +1,30 @@
+//var express = require('express');
+//var router = express.Router();
+var mongoose = require('mongoose');
+var model = require('../models/model');
+var multer  = require('multer');
+var upload = multer({ dest: './public/uploads' });
+
+var uploadFile = function(req, res, next) {
+    var article = new model({
+        title: req.body.title,
+        text: req.body.text,
+        //image: req.file.path + '.' + req.file.originalname.split('.').pop(),
+        image:  req.file.path.replace('public', ''),
+        author: "xxx"
+    });
+    article.save(function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('meow');
+            res.json(data);
+            //res.json(article);
+        }
+    });
+};
+
+module.exports = {
+    upload: uploadFile
+};
+
---
+++ Task07/readme.md
@@ -0,0 +1,16 @@
+# Task 7
+
+Installed globally:
++-- chai@3.4.1
++-- express-generator@4.13.1
++-- handlebars@2.0.0
++-- http-server@0.7.4
++-- UNMET PEER DEPENDENCY jasmine-core@*
++-- karma@0.13.22
++-- karma-chrome-launcher@1.0.1
++-- karma-jasmine@1.0.2
++-- karma-mocha@1.0.1
++-- UNMET PEER DEPENDENCY mocha@*
++-- nib@1.1.0
+L-- stylus@0.52.4
+  
